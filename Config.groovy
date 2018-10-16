1a478a8976ce1355ec22fe83ffbb059149809f73
Ralf D. Mueller <ralf.d.mueller@gmail.com>
2018-10-16 21:20:09 +0200

    splitted docs from master project

--- Config.groovy
+++ Config.groovy
@@ -9,7 +9,7 @@ outputPath = 'build/docs'
 // or in the command line, and therefore must be relative to it.
 inputPath = 'src/docs'
 
-inputFiles = [[file: 'test.adoc',              formats: ['html','pdf','docbook']],
+inputFiles = [[file: 'tutorials/tutorial1.adoc',         formats: ['html','pdf','docbook']],
               [file: 'manual.adoc',            formats: ['html','pdf']],
               [file: 'ppt/Demo.pptx.ad',       formats: ['html','revealjs']],
              ]
